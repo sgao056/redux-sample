a8679deb1831b66dfc09f37e6e2bab4af43349bb
sgao056 <sgao056@uottawa.ca>
2021-11-05 16:42:15 -0400

    function to class

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,47 +1,44 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { connect } from 'react-redux'
 
-const addToCart = (itemID) => {
-    return {
-        type: 'ADD_TO_CART',
-        payload: {
-            id:itemID
-        }
+class Product extends Component {
+    handleAdd = ()=>{
+        this.props.addToCart()
     }
-}
-const Product = ({productData}) => {
-    return(
-        <div className="row" 
-            style={{margin:'20px', 
-                    backgroundColor:'black', 
-                    color: '#eee',
-                    padding:'5px',
-                    height:'200px',
-                    fontSize:'20px',
-                    alignItems:'center'}}>
-            <div className="col-2" style={{height:'190px'}}>
-                <img src={productData.image} alt="" style={{height:'100%', width:'100%'}}/>    
-            </div>
-            <div className="col-2">
-                name: {productData.name}
-            </div>
-            <div className="col-3">
-                storage: 
-                <input type="text" value={productData.count} readOnly="readOnly"/>
-            </div>
-            <div className="col-2">
-                color: {productData.color}
-            </div>
-            <div className="col-3 row">
-                <div className="col-6">
-                    <button onClick={() => addToCart(productData.id)}>Add to Cart</button>
+    render(){
+        return(
+            <div className="row" 
+                style={{margin:'20px', 
+                        backgroundColor:'black', 
+                        color: '#eee',
+                        padding:'5px',
+                        height:'200px',
+                        fontSize:'20px',
+                        alignItems:'center'}}>
+                <div className="col-2" style={{height:'190px'}}>
+                    <img src={this.props.productData.image} alt="" style={{height:'100%', width:'100%'}}/>    
                 </div>
-                <div className="col-6">
-                    <button>???</button>
+                <div className="col-2">
+                    name: {this.props.productData.name}
+                </div>
+                <div className="col-3">
+                    storage: 
+                    <input type="text" value={this.props.productData.count} readOnly="readOnly"/>
+                </div>
+                <div className="col-2">
+                    color: {this.props.productData.color}
+                </div>
+                <div className="col-3 row">
+                    <div className="col-6">
+                        <button onClick={this.handleAdd}>Add to Cart</button>
+                    </div>
+                    <div className="col-6">
+                        <button>???</button>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 const mapDispatchToProps = (dispatch) => {
